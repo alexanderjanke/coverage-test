d7006691135a367aace5e0db55046145b2fff6fb
Alexander Janke <33967771+alexanderjanke@users.noreply.github.com>
2020-09-08 19:47:38 +0200

    more math

--- src/add.ts
+++ src/add.ts
@@ -1,3 +1,11 @@
 export function add(a: number, b: number): number {
   return a + b;
 }
+
+export function max(a: number, b: number): number {
+  if (a < b) {
+    return b;
+  } else {
+    return a;
+  }
+}
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,12 @@
 import test from "ava";
-import { add } from "../src/add";
+import { add, max } from "../src/add";
 
 test("1+2 = 3", (t) => {
   const sum = add(1, 2);
   t.is(sum, 3);
+});
+
+test("max", (t) => {
+  const res = max(5, 10);
+  t.is(res, 5);
 });
